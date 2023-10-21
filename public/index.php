a49428f4e285f23dc52494b8deeef3391edf440e
TUF <nguyenvo1373.hg@gmail.com>
2023-10-21 17:45:49 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../bootstrap.php';
+
+define('APPNAME', 'My Phonebook');
+
+session_start();
+
+$router = new \Bramus\Router\Router();
+
+// Auth routes
+$router->post('/logout', '\App\Controllers\Auth\LoginController@destroy');
+$router->get('/register', '\App\Controllers\Auth\RegisterController@create');
+$router->post('/register', '\\App\Controllers\Auth\RegisterController@store');
+$router->get('/login', '\App\Controllers\Auth\LoginController@create');
+$router->post('/login', '\App\Controllers\Auth\LoginController@store');
+
+// Contact routes
+$router->get('/', '\App\Controllers\HomeController@index');
+$router->get('/home', '\App\Controllers\HomeController@index');
+
+$router->run();
